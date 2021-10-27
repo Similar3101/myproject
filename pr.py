ec317605bdab6c2d83ed025673ee1b46793ffedb
Similar3101 <alecsander.mac@gmail.com>
2021-10-27 17:05:34 +0800

    ничего не добавил пока что

--- pr.py
+++ pr.py
@@ -1,2 +1,24 @@
-print('lolik')
-print(1)
+
+tel = input()
+if tel[0] == '8' or tel[0:2] == '+7':
+    tel = [a for a in tel if a not in ['(', ')', ' ', '-']]
+    if tel[0] == '8':
+        tel[0] = '+7'
+    print(''.join(tel))
+if '  ' not in tel:
+    tel = [a for a in tel if a not in ['(', ')', ' ', '-']]
+    if tel[0] == '8':
+        tel[0] = '+7'
+    print(''.join(tel))
+if '--' not in tel:
+    tel = [a for a in tel if a not in ['(', ')', ' ', '-']]
+    if tel[0] == '8':
+        tel[0] = '+7'
+    print(''.join(tel))
+elif tel.count('(') == tel.count(')'):
+    tel = [a for a in tel if a not in ['(', ')', ' ', '-']]
+    if tel[0] == '8':
+        tel[0] = '+7'
+    print(''.join(tel))
+else:
+    raise Exception('error')
